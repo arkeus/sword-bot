19948dcdc0049727e7ff84423aca590fc7158a76
Lee Miller <iarkeus@gmail.com>
2013-11-02 15:39:13 -0700

    Statistics class.

---
+++ src/io/arkeus/sword/user/Statistics.scala
@@ -0,0 +1,10 @@
+package io.arkeus.sword.user
+
+class Statistics {
+	var strength: Int = 10
+	var defense: Int = 10
+	var wisdom: Int = 10
+	var agility: Int = 10
+	var stamina: Int = 10
+	var spirit: Int = 10
+}
