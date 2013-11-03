ca1d9730a369d6b3ef6c938d5529144f78c225a7
Lee Miller <iarkeus@gmail.com>
2013-11-02 17:57:17 -0700

    Class for auto colorizing messages.

---
+++ src/io/arkeus/sword/user/message/Colorizer.scala
@@ -0,0 +1,31 @@
+package io.arkeus.sword.user.message
+
+import org.jibble.pircbot.Colors
+
+object Colorizer {
+	val bracketedColors = Array(Colors.RED, Colors.OLIVE, Colors.GREEN, Colors.DARK_GREEN, Colors.BLUE, Colors.DARK_BLUE)
+	var bracketOffset = 0
+	
+	def colorize(message:String) = {
+		//"""\[(.*?)\]""".r.findAllIn(message).map(colorizeBrackets).mkString(" ")
+		var coloredMessage = message
+		for (brackets <- """\[(.*?)\]""".r.findAllIn(message)) {
+			coloredMessage = colorizeBrackets(coloredMessage, brackets)
+		}
+		formatTags(coloredMessage)
+	}
+	
+	def colorizeBrackets(message:String, brackets:String) = {
+		val color = bracketedColors(bracketOffset % bracketedColors.length)
+		bracketOffset += 1
+		val inner = brackets.substring(1, brackets.length - 1)
+		val split = inner.indexOf(" ")
+		println("SPLIT " + split)
+		val alias = if (split < 0) inner else inner.substring(0, split)
+		val rest = (if (split < 0) "" else inner.substring(split))
+		val coloredBrackets = Colors.BOLD + color + "[" + Colors.NORMAL + Colors.BOLD + alias + Colors.BOLD + rest + Colors.BOLD + color + "]" + Colors.NORMAL
+		message.replace(brackets, coloredBrackets)
+	}
+	
+	def formatTags(message:String) = message.replaceAll("""''(.*?)''""", Colors.BOLD + "$1" + Colors.BOLD)
+}
